e20001c4ee6764b32e0f92ab2a249b3f358203cc
Дмитриев.Алексей <.@TERM1>
2025-02-12 12:38:01 +0300

    Созданы и подключены словари ресурсов

--- View/Window/LoginWindow.xaml.cs
+++ View/Window/LoginWindow.xaml.cs
@@ -1,23 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
-namespace Bookmaster.View.Window
+﻿namespace Bookmaster.View.Window
 {
     /// <summary>
     /// Логика взаимодействия для LoginWindow.xaml
     /// </summary>
-    public partial class LoginWindow : Window
+    public partial class LoginWindow : System.Windows.Window
     {
         public LoginWindow()
         {
